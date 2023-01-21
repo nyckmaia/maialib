97d38bb822004c43b035e485fd0cac2839d49fee
Nycholas Maia <nyckmaia@gmail.com>
2023-01-21 17:34:27 -0300

    WIP - Step 2
    - Add scripts/*.py
    
    Signed-off-by: Nycholas Maia <nyckmaia@gmail.com>

--- scripts/make-install.py
+++ scripts/make-install.py
@@ -12,18 +12,25 @@ distDir = "dist"
 os.system(f"pip install --user {distDir}/")
 
 print(f"{color.OKGREEN}Generating Python Module Stubs...{color.ENDC}")
-genStubsCommand = "coverage run -m pybind11_stubgen maialib --output-dir=\"./stubs\" --root-module-suffix="" --ignore-invalid=all --no-setup-py"
+# genStubsCommand = "coverage run -m pybind11_stubgen maialib --output-dir=\"./stubs\" --root-module-suffix="" --ignore-invalid=all --no-setup-py"
+genStubsCommand = "pybind11-stubgen maialib --output-dir=\"./stubs\" --root-module-suffix="" --ignore-invalid=all --no-setup-py"
+os.system(genStubsCommand)
+genStubsCommand = "pybind11-stubgen maialib.maiacore --output-dir=\"./stubs\" --root-module-suffix="" --ignore-invalid=all --no-setup-py"
+os.system(genStubsCommand)
+genStubsCommand = "pybind11-stubgen maialib.maiapy --output-dir=\"./stubs\" --root-module-suffix="" --ignore-invalid=all --no-setup-py"
 os.system(genStubsCommand)
 
 # Copy stubs files to the 'dist' folder
 copy2("./stubs/maialib/__init__.pyi", f"{distDir}/maialib/__init__.pyi")
-copytree("./stubs/maialib/maiacore/", f"{distDir}/maialib/maiacore/", dirs_exist_ok=True)
-copytree("./stubs/maialib/maiapy/", f"{distDir}/maialib/maiapy/", dirs_exist_ok=True)
+copytree("./stubs/maialib/maiacore/",
+         f"{distDir}/maialib/maiacore/", dirs_exist_ok=True)
+copytree("./stubs/maialib/maiapy/",
+         f"{distDir}/maialib/maiapy/", dirs_exist_ok=True)
 
 # Uninstall maialib
 os.system(f"pip uninstall --yes maialib")
 
-# Re-install maialib, now with Python stubs
+# Re - install maialib, now with Python stubs
 os.system(f"pip install --user {distDir}/")
 
 print(f"{color.OKGREEN}Maialib Installed on Python kernel v{platform.python_version()} {color.ENDC}")
--- scripts/make-module.py
+++ scripts/make-module.py
@@ -7,7 +7,8 @@ from terminal_colors import *
 numArgs = len(sys.argv)
 
 if (numArgs != 2):
-    print(f"{color.FAIL}[ERROR] You MUST pass 1 argument: 'buildType' -> Debug or Release{color.ENDC}")
+    print(
+        f"{color.FAIL}[ERROR] You MUST pass 1 argument: 'buildType' -> Debug or Release{color.ENDC}")
 
 # Get input command line arguments
 buildType = sys.argv[1]
@@ -17,25 +18,39 @@ print(f"{color.OKGREEN}Building maiacore Python module on {buildType} mode...{co
 # Get the Operational System
 myOS = platform.system()
 
-# Create a 'build' folder (if not exists)
+# Create a 'build' folder(if not exists)
 path = Path.cwd() / "build" / myOS / "module"
 path.mkdir(parents=True, exist_ok=True)
 
 # Base CMake command to build the python module
-cmakeCommand = f"cmake -G \"Unix Makefiles\" -B {path} -S ./core -D PYBIND_LIB=ON -DCMAKE_BUILD_TYPE={buildType}"
+if myOS == "Windows":
+    vcvarsallPath = "\"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat\""
+    os.system(f"{vcvarsallPath} x86_amd64")
+
+    clPath = "\"C:\\Program Files\\Microsoft Visual Studio\\2022\Community\\VC\\Tools\\MSVC\\14.34.31933\\bin\\Hostx64\\x64\\cl.exe\""
+    cmakeCommand = f"cmake -G \"Visual Studio 17 2022\" -A x64 -B {path} -S . -D PYBIND_LIB=ON -DCMAKE_BUILD_TYPE={buildType} -DCMAKE_CXX_COMPILER={clPath} -DMAIALIB_VERSION_INFO=0.0.0"
+else:
+    cmakeCommand = f"cmake -G \"Unix Makefiles\" -B {path} -S . -D PYBIND_LIB=ON -DCMAKE_BUILD_TYPE={buildType}"
 
 if (buildType == "Debug" or buildType == "Release"):
     cmakeCommand += " -DPROFILING=ON"
 
-# If 'Windows' define the MinGW 'make.exe'
-if myOS == "Windows":
-    cmakeCommand += ' -DCMAKE_MAKE_PROGRAM="C:/msys64/mingw64/bin/make.exe"'
+# #If 'Windows' define the MinGW 'make.exe'
+# if myOS == "Windows":
+#cmakeCommand += ' -DCMAKE_MAKE_PROGRAM="C:/msys64/mingw64/bin/make.exe"'
 
 # Get CPU num threads
 numThreads = os.cpu_count()
 
 # Run CMake and Make commands
 os.system(cmakeCommand)
-os.system(f"make -j {numThreads} -C {path} --no-print-directory")
+if myOS == "Windows":
+    msbuildPath = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\BuildTools\\MSBuild\\Current\\Bin"
+    os.system(
+        f"msbuild.exe {path}\\maiacore.sln -verbosity:quiet -maxcpucount")
+
+# os.system(f "cmake --build {path} --config Debug")
+else:
+    os.system(f"make -j {numThreads} -C {path} --no-print-directory")
 
 print(f"{color.OKGREEN}Done!{color.ENDC}")
